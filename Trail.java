19710fd16dd55ff3c720044fd9531311ffde375a
janardhan-kantubhukta <85569180+janardhan-kantubhukta@users.noreply.github.com>
2024-01-03 19:04:56 +0530

    Add files via upload

---
+++ Trail.java
@@ -0,0 +1,55 @@
+import java.lang.*;
+import java.util.*;
+class Trail
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char op;
+        int ans=0;
+        int a,b;
+        while(true)
+        {  
+            System.out.print("Enter the operator:");
+            op=sc.next().trim().charAt(0);
+            if(op=='+' || op=='-'||op=='*'||op=='/'|| op=='%')
+            {
+                System.out.print("Enter the values of a and b:");
+                a=sc.nextInt();
+                b=sc.nextInt();
+                switch(op)
+                {
+                    case '+':
+                    System.out.println("Result:"+(a+b));
+                    break;
+
+                    case '-':
+                    System.out.println("Result:"+(a-b));
+                    break;
+
+                    case '*':
+                    System.out.println("Result:"+(a*b));
+                    break;
+
+                    case '/':
+                    System.out.println("Result:"+(a/b));
+                    break;
+                    
+                    case  '%':
+                    System.out.println("Result:"+(a%b));
+                    break;
+
+                    default:
+                    System.out.println("Enter the valid intput");
+                }
+
+            }
+        else{
+            System.out.print("Thanks for Using");
+            break;
+        }
+        }
+
+    }
+
+}
